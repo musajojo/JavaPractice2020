564ddf5ead505bbcc429243101121bead984d346
Moses Dee <58441332+musajojo@users.noreply.github.com>
2020-06-19 21:28:25 -0400

    first commit

---
+++ src/main/java/lottery/Lottery01.java
@@ -0,0 +1,17 @@
+package lottery;
+
+import java.util.Scanner;
+
+public class Lottery01 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Pick a number");
+        int numberPicker = scan.nextInt();
+
+        System.out.println(numberPicker);
+
+    }
+}
